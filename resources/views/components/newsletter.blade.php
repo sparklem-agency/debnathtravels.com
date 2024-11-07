c73c95779f10d077c65cbd174eac6a3778968780
joydeep-bhowmik <bhowmikj9432@gmail.com>
2024-11-07 16:49:43 +0530

    wip

--- resources/views/components/newsletter.blade.php
+++ resources/views/components/newsletter.blade.php
@@ -8,7 +8,7 @@
         <form class="mt-10 text-sm" action="" method="get">
             <div
                 class="mx-auto grid max-w-xs grid-cols-[auto_150px] items-center overflow-hidden rounded-full !border-0 bg-white p-1 focus-within:ring-2">
-                <input class="border-none p-3 !outline-none !ring-0" id="" name="" type="email"
+                <input class="w-full border-none p-3 !outline-none !ring-0" id="" name="" type="email"
                     placeholder="Email">
                 <button class="rounded-full bg-blue-700 p-3 font-bold text-white" type="submit">
                     subscribe

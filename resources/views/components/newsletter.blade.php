221e1b3cee3e88d3bf5af4b953dac776af17ea82
joydeep-bhowmik <bhowmikj9432@gmail.com>
2024-11-07 16:44:33 +0530

    wip

--- resources/views/components/newsletter.blade.php
+++ resources/views/components/newsletter.blade.php
@@ -7,10 +7,10 @@
         </h2>
         <form class="mt-10 text-sm" action="" method="get">
             <div
-                class="mx-auto flex max-w-xs items-center overflow-hidden rounded-full !border-0 bg-white p-1 focus-within:ring-2">
+                class="mx-auto grid max-w-xs grid-cols-[auto_150px] items-center overflow-hidden rounded-full !border-0 bg-white p-1 focus-within:ring-2">
                 <input class="border-none p-3 !outline-none !ring-0" id="" name="" type="email"
                     placeholder="Email">
-                <button class="ml-auto rounded-full bg-blue-700 p-3 font-bold text-white" type="submit">
+                <button class="rounded-full bg-blue-700 p-3 font-bold text-white" type="submit">
                     subscribe
                 </button>
             </div>

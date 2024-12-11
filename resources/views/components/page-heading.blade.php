d9908085d75f5d883864939511505b42c229cfb6
joydeep-bhowmik <bhowmikj9432@gmail.com>
2024-12-11 16:49:55 +0530

    wip

--- resources/views/components/page-heading.blade.php
+++ resources/views/components/page-heading.blade.php
@@ -13,7 +13,7 @@
                     d="M12.1717 12.0005L9.34326 9.17203L10.7575 7.75781L15.0001 12.0005L10.7575 16.2431L9.34326 14.8289L12.1717 12.0005Z">
                 </path>
             </svg>
-            <a class="block whitespace-nowrap">{{ $heading ?? $slot }}</a>
+            <a class="block whitespace-nowrap max-w-3xl mx-auto">{{ $heading ?? $slot }}</a>
 
         </div>
     </div>

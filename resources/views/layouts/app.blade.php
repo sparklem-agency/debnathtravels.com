8f41d96712237bd6b5928234c8bf662401c3f854
joydeep-bhowmik <bhowmikj9432@gmail.com>
2025-02-08 12:56:42 +0530

    google analystic added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,20 @@
 
 <head>
 
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-0E4K37F2C6"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-0E4K37F2C6');
+    </script>
+
+
     <!-- Google Tag Manager -->
     <script>
         (function(w, d, s, l, i) {
@@ -49,7 +63,11 @@
     @livewireStyles
 </head>
 
+
+
 <body>
+
+
 
     <!-- Google Tag Manager (noscript) -->
     <noscript>

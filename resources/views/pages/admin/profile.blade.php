29caef6a3e0e0240ced96690751f54eb747047c8
joydeep-bhowmik <bhowmikj9432@gmail.com>
2025-02-21 12:12:57 +0530

    wi

--- resources/views/pages/admin/profile.blade.php
+++ resources/views/pages/admin/profile.blade.php
@@ -7,28 +7,28 @@ name('profile');
 ?>
 
 <x-admin-layout title="Profile">
-    <x-header size="lg" title="Profile">
+    <x-header size="lg" title="Profile" />
 
 
-        <div class="py-8">
-            <div class="space-y-5">
-                <x-card>
-                    <div class="max-w-xl">
-                        <livewire:profile.update-profile-information-form />
-                    </div>
-                </x-card>
+    <div class="py-8">
+        <div class="space-y-5">
+            <x-card>
+                <div class="max-w-xl">
+                    <livewire:profile.update-profile-information-form />
+                </div>
+            </x-card>
 
-                <x-card>
-                    <div class="max-w-xl">
-                        <livewire:profile.update-password-form />
-                    </div>
-                </x-card>
+            <x-card>
+                <div class="max-w-xl">
+                    <livewire:profile.update-password-form />
+                </div>
+            </x-card>
 
-                <x-card>
-                    <div class="max-w-xl">
-                        <livewire:profile.delete-user-form />
-                    </div>
-                </x-card>
-            </div>
+            <x-card>
+                <div class="max-w-xl">
+                    <livewire:profile.delete-user-form />
+                </div>
+            </x-card>
         </div>
+    </div>
 </x-admin-layout>
